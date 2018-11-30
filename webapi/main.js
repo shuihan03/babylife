93c4f82db2c9978dacf520786d5dadb33a9f9234
shuihan <gaojian0614@163.com>
2018-11-30 16:45:34 +0800

    添加html页面导航,上传文件方法

--- webapi/main.js
+++ webapi/main.js
@@ -5,6 +5,7 @@ let path=require('path')
 let express = require('express');
 let app = express();
 let userRoot=require('./user');
+let pageRoot=require('./pages')
 
 //打印日志
 let accessLogStream = fs.createWriteStream(path.join(__dirname, '../log/access.log'), {flags: 'a'});
@@ -13,9 +14,19 @@ app.use(morgan('combined', {stream: accessLogStream}));
 
 //注册路由
 app.use('/user',userRoot);
+app.use('/view',pageRoot);
+//处理静态文件，html，css ,js,image等;url 不用输入public;http://localhost/logo.png
+app.use(express.static('../public'));
 app.get('',function(req,res){
   res.end("hello world");
 })
+
+
+var bodyParser = require('body-parser');
+var multer  = require('multer');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: '/tmp/'}).array('image'));
+
 var server = app.listen(8081, function () {
 
   var host = server.address().address
---
+++ webapi/pages.js
@@ -0,0 +1,36 @@
+let express = require("express");
+let path=require('path')
+let router = express.Router();
+
+
+
+router.get('/index.htm', function (req, res) {
+    res.sendFile( path.join(__dirname,'../views/index.htm'));
+  })
+  router.get('/error.htm', function (req, res) {
+    res.sendFile( path.join(__dirname,'../views/error.htm'));
+  })
+  router.get('/upload.htm', function (req, res) {
+    res.sendFile( path.join(__dirname,'../views/upload.htm'));
+  })
+  router.post('/file_upload', function (req, res) {
+ 
+    console.log(req.files[0]);  // 上传的文件信息
+  
+    var des_file = __dirname + "/" + req.files[0].originalname;
+    fs.readFile( req.files[0].path, function (err, data) {
+         fs.writeFile(des_file, data, function (err) {
+          if( err ){
+               console.log( err );
+          }else{
+                response = {
+                    message:'File uploaded successfully', 
+                    filename:req.files[0].originalname
+               };
+           }
+           console.log( response );
+           res.end( JSON.stringify( response ) );
+        });
+    });
+ })
+  module.exports=router;
